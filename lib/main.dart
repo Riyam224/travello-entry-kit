aeb6948828446da77c39bfd23c05f3d29e2ad824
Riyam224 <cs50riyam211@gmail.com>
2025-03-26 23:39:33 +0300

    add firebase configure and services classes

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:travello/core/app_routes.dart';
 import 'package:travello/features/welcome/presentation/views/welcome_view.dart';
 import 'package:travello/services/shared_prefs.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Travello());
 }
 
